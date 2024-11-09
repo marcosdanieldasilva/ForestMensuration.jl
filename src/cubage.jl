eda9abec1102c42e17c540396c7f4151ff2306b3
marcos <marcosdasilva@5a.tec.br>
2024-11-09 17:22:39 -0300

    cubage tests 0.1

--- src/cubage.jl
+++ src/cubage.jl
@@ -1,27 +1,32 @@
-function _diameter_interpolation(h0::Real, h::Vector{<:Real}, d::Vector{<:Real}) :: Float64
+function _diameter_interpolation(h0::Real, h::Vector{<:Real}, d::Vector{<:Real})::Float64
+  if h0 < h[1] || h0 > h[end]
+    error("Height h0 is outside the range of heights in the data.")
+  end
   n = length(h)
   for i in 2:n
     if h0 <= h[i]
-      d_prev = d[i - 1]  # Diameter at the lower height
-      d_next = d[i]    # Diameter at the higher height
-      h_prev = h[i - 1]  # Lower height
-      h_next = h[i]    # Higher height
+      d_prev = d[i-1]  # Diameter at the lower height
+      d_next = d[i]      # Diameter at the higher height
+      h_prev = h[i-1]  # Lower height
+      h_next = h[i]      # Higher height
       # Perform linear interpolation
       return d_prev + ((d_next - d_prev) * (h0 - h_prev)) / (h_next - h_prev)
     end
   end
-  error("Height h0 is outside the range of heights in the data.")
 end
 
 # Function to interpolate the height at a given diameter
-function _height_interpolation(d_limit::Real, h::Vector{<:Real}, d::Vector{<:Real}) :: Tuple{Float64, Int}
+function _height_interpolation(d_limit::Real, h::Vector{<:Real}, d::Vector{<:Real})::Tuple{Float64,Int}
+  if d_limit > d[1] || d_limit < d[end]
+    error("Diameter d_limit is outside the range of diameters in the data.")
+  end
   n = length(d)
   # Find the position where d_limit should be interpolated
   for i in 2:n
-    if d_limit <= d[i - 1] && d_limit >= d[i]
-      h_prev = h[i - 1]  # Height at the lower diameter
+    if d_limit <= d[i-1] && d_limit >= d[i]
+      h_prev = h[i-1]  # Height at the lower diameter
       h_next = h[i]  # Height at the higher diameter
-      d_prev = d[i - 1]  # Lower diameter
+      d_prev = d[i-1]  # Lower diameter
       d_next = d[i]  # Higher diameter
       # Perform linear interpolation
       interpolated_height = h_prev + ((h_next - h_prev) * (d_limit - d_prev)) / (d_next - d_prev)
@@ -29,7 +34,6 @@ function _height_interpolation(d_limit::Real, h::Vector{<:Real}, d::Vector{<:Rea
     end
   end
 
-  error("Diameter d_limit is outside the range of diameters in the data.")
 end
 
 """
@@ -41,8 +45,16 @@ Calculates the volume of a cylinder, used to estimate the volume (v0) of the tre
 
 # Returns
 - `Float64`: The volume of the cylinder in cubic meters.
+
+# Throws
+- `ArgumentError`: If `h` or `d` is less than or equal to zero, as height and diameter must be positive values.
 """
-@inline cylinder_volume(h::Real, d::Real) :: Float64 = (π / 40000) * d^2 * h
+@inline function cylinder_volume(h::Real, d::Real)::Float64
+  if h <= 0 || d <= 0
+    throw(ArgumentError("Height and diameter must be positive values."))
+  end
+  return (π / 40000) * d^2 * h
+end
 
 """
 Calculates the volume of a cone, used to estimate the final portion (vn) of the tree, typically considered to have a conical shape.
@@ -53,8 +65,16 @@ Calculates the volume of a cone, used to estimate the final portion (vn) of the 
 
 # Returns
 - `Float64`: The volume of the cone in cubic meters.
+
+# Throws
+- `ArgumentError`: If `h` or `d` is less than or equal to zero, as height and diameter must be positive values.
 """
-@inline cone_volume(h::Real, d::Real) :: Float64 = (1 / 3) * (π / 40000) * d^2 * h
+@inline function cone_volume(h::Real, d::Real)::Float64
+  if h <= 0 || d <= 0
+    throw(ArgumentError("Height and diameter must be positive values."))
+  end
+  return (1 / 3) * (π / 40000) * d^2 * h
+end
 
 """
 Calculates the bark factor, used to estimate the volume without bark.
@@ -66,7 +86,7 @@ The bark factor is used to estimate the volume without bark by considering the r
 - `e::Vector{<:Real}`: Vector of bark thicknesses in millimeters.
 
 # Returns
-- `Float64`: The bark factor.
+- `Float64`: The bark factor, which represents the proportion of the diameter without bark.
 
 # Example
 # Calculate bark factor
@@ -75,7 +95,15 @@ k = bark_factor(d, e)
 # Calculate total volume without bark
 total_volume_without_bark = total_volume * k^2
 """
-@inline bark_factor(d::Vector{<:Real}, e::Vector{<:Real}) :: Float64 = 1 - (sum(e) / sum(d))
+@inline function bark_factor(d::Vector{<:Real}, e::Vector{<:Real})::Float64
+  if any(x -> x < 0, d)
+    throw(ArgumentError("Diameters must be positive"))
+  elseif any(x -> x < 0, e)
+    throw(ArgumentError("bark thickness must be non-negative."))
+  else
+    return 1 - (sum(e) / sum(d))
+  end
+end
 
 """
 Calculate tree bole volume cubic meters using Smalian, Newton, or Huber methods.
@@ -90,19 +118,29 @@ In each section, diameters and lengths are measured at positions that vary accor
 # Returns
 - `Float64`: The volume of the bole in cubic meters.
 """
-@inline bole_volume(method::Type{<:CubingMethod}, h::Vector{<:Real}, d::Vector{<:Real}) :: Float64 = bole_volume(method, h, d)
+@inline function bole_volume(method::Type{<:CubingMethod}, h::Vector{<:Real}, d::Vector{<:Real})::Float64
+  # Check if h and d have the same length
+  if length(h) != length(d)
+    throw(ArgumentError("Vectors h and d must have the same length."))
+    # Check if all elements in h and d are positive
+  elseif any(x -> x < 0, h) || any(x -> x < 0, d)
+    throw(ArgumentError("All elements in vectors h and d must be non-negative."))
+  else # Proceed to calculate volume using _bole_volume
+    return _bole_volume(method, h, d, 2, length(h))
+  end
+end
 
 # Smalian Method for Bole Volume Calculation
-@inline function bole_volume(::Type{<:Smalian}, h::Vector{<:Real}, d::Vector{<:Real}, start_idx::Int, end_idx::Int) :: Float64
-  return map(i -> (π / 40000) * ((d[i]^2 + d[i - 1]^2) / 2) * (h[i] - h[i - 1]), start_idx:end_idx) |> sum
+@inline function _bole_volume(::Type{<:Smalian}, h::Vector{<:Real}, d::Vector{<:Real}, start_idx::Int, end_idx::Int)::Float64
+  return map(i -> (π / 40000) * ((d[i]^2 + d[i-1]^2) / 2) * (h[i] - h[i-1]), start_idx:end_idx) |> sum
 end
 # Huber Method for Bole Volume Calculation
-@inline function bole_volume(::Type{<:Huber}, h::Vector{<:Real}, d::Vector{<:Real}, start_idx::Int, end_idx::Int) :: Float64
-  cylinder_volume.(map(i -> h[i] - h[i - 2], start_idx + 1:2:end_idx+1), map(i -> d[i], start_idx:2:end_idx)) |> sum
+@inline function _bole_volume(::Type{<:Huber}, h::Vector{<:Real}, d::Vector{<:Real}, start_idx::Int, end_idx::Int)::Float64
+  cylinder_volume.(map(i -> h[i] - h[i-2], start_idx+1:2:end_idx+1), map(i -> d[i], start_idx:2:end_idx)) |> sum
 end
 # Newton Method for Bole Volume Calculation
-@inline function bole_volume(::Type{<:Newton}, h::Vector{<:Real}, d::Vector{<:Real}, start_idx::Int, end_idx::Int) :: Float64
-  map(i -> (π / 240000) * (h[i + 2] - h[i]) * (d[i]^2 + 4 * d[i + 1]^2 + d[i + 2]^2), start_idx:2:(end_idx - 2)) |> sum
+@inline function _bole_volume(::Type{<:Newton}, h::Vector{<:Real}, d::Vector{<:Real}, start_idx::Int, end_idx::Int)::Float64
+  map(i -> (π / 240000) * (h[i+2] - h[i]) * (d[i]^2 + 4 * d[i+1]^2 + d[i+2]^2), start_idx:2:(end_idx-2)) |> sum
 end
 
 """
@@ -122,7 +160,12 @@ Where:
 # Returns
 - `Float64`: The artificial form factor.
 """
-@inline artificial_form_factor(vt::Real, ht::Real, dbh::Real) :: Float64 = vt / cylinder_volume(ht, dbh)
+@inline function artificial_form_factor(vt::Real, ht::Real, dbh::Real)::Float64
+  if vt <= 0
+    throw(ArgumentError("Volume must be positive."))
+  end
+  return vt / cylinder_volume(ht, dbh)
+end
 
 """
 Natural Form Factor (nff):
@@ -143,7 +186,12 @@ Interpolate diameter at a given height using linear interpolation.
 # Returns
 - `Float64`: The natural form factor.
 """
-@inline natural_form_factor(vt::Real, ht::Real, h::Vector{<:Real}, d::Vector{<:Real}) :: Float64 = vt / cylinder_volume(h[end], _diameter_interpolation(ht * 0.1, h, d))
+@inline function natural_form_factor(vt::Real, ht::Real, h::Vector{<:Real}, d::Vector{<:Real})::Float64
+  if vt <= 0
+    throw(ArgumentError("Volume must be positive."))
+  end
+  return vt / cylinder_volume(h[end], _diameter_interpolation(ht * 0.1, h, d))
+end
 
 """
 Form Quotient (qf):
@@ -165,7 +213,12 @@ Similar to the form factor, the volume of a tree, with or without bark, can be o
 # Returns
 - `Float64`: The form quotient.
 """
-@inline quotient_form(ht::Real, dbh::Real, h::Vector{<:Real}, d::Vector{<:Real}) :: Float64 = _diameter_interpolation(ht * 0.5, h, d) / dbh
+@inline function quotient_form(ht::Real, dbh::Real, h::Vector{<:Real}, d::Vector{<:Real})::Float64 
+  if dbh <= 0
+    throw(ArgumentError("Diameter must be positive."))
+  end
+  return _diameter_interpolation(ht * 0.5, h, d) / dbh
+end
 
 """
 Calculate tree cubage using Smalian, Newton, or Huber methods.
@@ -184,10 +237,13 @@ Determination can be carried out on felled trees or standing trees using equipme
 # Returns
 - `DataFrame`: A DataFrame with the calculated volumes and form factors.
 """
-function cubage(method::Type{<:CubingMethod}, h::Vector{<:Real}, d::Vector{<:Real}, d_limit::Union{Real, Nothing}=nothing; dbh::Real=1.3) :: DataFrame
+function cubage(method::Type{<:CubingMethod}, h::Vector{<:Real}, d::Vector{<:Real}, d_limit::Union{Real,Nothing}=nothing; dbh::Real=1.3)::DataFrame
+  if length(h) != length(d)
+    throw(ArgumentError("Vectors h and d must have the same length."))
+  end
   # Find position where h = 1.3
   idx = findfirst(isequal(dbh), h)
-  
+
   if idx === nothing
     error("Value of dbh when h = $dbh not found.")
   else
@@ -199,15 +255,15 @@ function cubage(method::Type{<:CubingMethod}, h::Vector{<:Real}, d::Vector{<:Rea
 
   # If commercial diameter is not provided, set it to the last value in the diameter vector
   if d_limit === nothing
-    d_limit = d[end - 1]
+    d_limit = d[end-1]
   end
-  
+
   if d_limit > maximum(d)
     # If no commercial limit or the limit is greater than any diameter, consider all as residual
-    hc = h[end - 1]
+    hc = h[end-1]
     hc_idx = length(h) - 1
     vc = 0.0  # No commercial bole volume
-    vr = bole_volume(method, h, d, 2, hc_idx)  # All bole volume is residual
+    vr = _bole_volume(method, h, d, 2, hc_idx)  # All bole volume is residual
   else
     if d_limit ∉ d && d_limit >= minimum(d)
       # Interpolate to find the corresponding height for the commercial diameter limit
@@ -217,19 +273,19 @@ function cubage(method::Type{<:CubingMethod}, h::Vector{<:Real}, d::Vector{<:Rea
       insert!(h, hc_idx, hi_at_d_limit)
       insert!(d, hc_idx, d_limit)
       hc = h[hc_idx]
-    elseif  d_limit ∈ d
+    elseif d_limit ∈ d
       hc_idx = findlast(isequal(d_limit), d)
       hc = h[hc_idx]
     else
       hc_idx = length(h) - 1
       hc = h[hc_idx]
     end
-    vc = bole_volume(method, h, d, 2, hc_idx)  # Commercial bole volume up to d_limit
-    vr = bole_volume(method, h, d, hc_idx + 1, length(h) - 1)  # Residual bole volume above d_limit
+    vc = _bole_volume(method, h, d, 2, hc_idx)  # Commercial bole volume up to d_limit
+    vr = _bole_volume(method, h, d, hc_idx + 1, length(h) - 1)  # Residual bole volume above d_limit
   end
   # Calculate other volumes
   v0 = cylinder_volume(h[begin], d[begin])  # Volume of the cylinder at the base
-  vn = cone_volume(ht - h[end - 1], d[end - 1])  # Volume of the cone above h[end - 1]
+  vn = cone_volume(ht - h[end-1], d[end-1])  # Volume of the cone above h[end - 1]
   vt = v0 + vc + vr + vn  # Total volume
   # Calculate the Form Factors
   aff = artificial_form_factor(vt, ht, dbh)
@@ -237,17 +293,17 @@ function cubage(method::Type{<:CubingMethod}, h::Vector{<:Real}, d::Vector{<:Rea
   qf = quotient_form(ht, dbh, h, d)
   # Create DataFrame with results
   DataFrame(
-    vt = vt,
-    v0 = v0,
-    vc = vc,
-    vr = vr,
-    vn = vn,
-    dbh = dbh,
-    ht = ht,
-    hc = hc,
-    aff = aff,
-    nff = nff,
-    qf = qf
+    vt=vt,
+    v0=v0,
+    vc=vc,
+    vr=vr,
+    vn=vn,
+    dbh=dbh,
+    ht=ht,
+    hc=hc,
+    aff=aff,
+    nff=nff,
+    qf=qf
   )
 end
 
@@ -267,20 +323,20 @@ In each section, diameters and lengths are measured at positions that vary accor
 # Returns
 - `DataFrame`: A DataFrame with the calculated volumes, form factors, and bark-adjusted volumes.
 """
-function cubage(method::Type{<:CubingMethod}, h::Vector{<:Real}, d::Vector{<:Real}, e::Vector{<:Real}, d_limit::Union{Real, Nothing}=nothing; dbh::Float64=1.3)
-  cubage_table = cubage(method, h, d, d_limit, dbh = dbh)
+function cubage(method::Type{<:CubingMethod}, h::Vector{<:Real}, d::Vector{<:Real}, e::Vector{<:Real}, d_limit::Union{Real,Nothing}=nothing; dbh::Float64=1.3)
+  cubage_table = cubage(method, h, d, d_limit, dbh=dbh)
   # bark factor
   insertcols!(cubage_table, :k => bark_factor(d, e))
   # total volume without bark
-  insertcols!(cubage_table, :vtwb => cubage_table.vt .* cubage_table.k.^2)
+  insertcols!(cubage_table, :vtwb => cubage_table.vt .* cubage_table.k .^ 2)
   # cylinder volume without bark
-  insertcols!(cubage_table, :v0wb => cubage_table.v0 .* cubage_table.k.^2)
+  insertcols!(cubage_table, :v0wb => cubage_table.v0 .* cubage_table.k .^ 2)
   # bole volume without bark
-  insertcols!(cubage_table, :vcwb => cubage_table.vc .* cubage_table.k.^2)
+  insertcols!(cubage_table, :vcwb => cubage_table.vc .* cubage_table.k .^ 2)
   # residual volume without bark
-  insertcols!(cubage_table, :vrwb => cubage_table.vr .* cubage_table.k.^2)
+  insertcols!(cubage_table, :vrwb => cubage_table.vr .* cubage_table.k .^ 2)
   # cone volume without bark
-  insertcols!(cubage_table, :vnwb => cubage_table.vn .* cubage_table.k.^2)
+  insertcols!(cubage_table, :vnwb => cubage_table.vn .* cubage_table.k .^ 2)
   return cubage_table
 end
 
@@ -301,9 +357,9 @@ In each section, diameters and lengths are measured at positions that vary accor
 # Returns
 - `DataFrame`: A DataFrame with the calculated volumes and form factors for each tree.
 """
-function cubage(method::Type{<:CubingMethod}, tree::Symbol, h::Symbol, d::Symbol, data::AbstractDataFrame, d_limit::Union{Real, Nothing}=nothing; dbh::Float64=1.3)
+function cubage(method::Type{<:CubingMethod}, tree::Symbol, h::Symbol, d::Symbol, data::AbstractDataFrame, d_limit::Union{Real,Nothing}=nothing; dbh::Float64=1.3)
   combine(groupby(data, tree)) do df
-    cubage(method, df[:, h], df[:, d], d_limit, dbh = dbh)
+    cubage(method, df[:, h], df[:, d], d_limit, dbh=dbh)
   end
 end
 
@@ -325,8 +381,8 @@ In each section, diameters and lengths are measured at positions that vary accor
 # Returns
 - `DataFrame`: A DataFrame with the calculated volumes, form factors, and bark-adjusted volumes for each tree.
 """
-function cubage(method::Type{<:CubingMethod}, tree::Symbol, h::Symbol, d::Symbol, e::Symbol, data::AbstractDataFrame, d_limit::Union{Real, Nothing}=nothing; dbh::Float64=1.3)
+function cubage(method::Type{<:CubingMethod}, tree::Symbol, h::Symbol, d::Symbol, e::Symbol, data::AbstractDataFrame, d_limit::Union{Real,Nothing}=nothing; dbh::Float64=1.3)
   combine(groupby(data, tree)) do df
-  cubage(method, df[:, h], df[:, d], df[:, e], d_limit, dbh = dbh)
+    cubage(method, df[:, h], df[:, d], df[:, e], d_limit, dbh=dbh)
   end
 end
