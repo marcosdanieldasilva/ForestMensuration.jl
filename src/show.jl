5740fadaa233012045edf91a2486c7ff442f9193
marcos <marcosdasilva@5a.tec.br>
2024-12-08 19:23:25 -0300

    remove ModelEquation

--- src/show.jl
+++ src/show.jl
@@ -1,16 +1,10 @@
 import Base: show
 
-# Define the show method for custom display of ModelEquation
-function show(io::IO, model::ModelEquation)
-  print(io, model.output)
-end
-
 # Custom show method for SiteAnalysis to display  the site_table and site_plot
 function show(io::IO, analysis::SiteAnalysis)
   show(io, analysis.site_table)
   display(analysis.site_plot)
 end
-
 
 _coefnames(model::FittedLinearModel) = vcat("β0", string.(StatsModels.coefnames(model.formula.rhs.terms[2:end])))
 
