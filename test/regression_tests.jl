13a0da48d31b72ec651f5bf4ff90300e9dd4fca4
marcos <marcosdasilva@5a.tec.br>
2025-01-23 15:49:06 -0300

    fix reg tests

--- test/regression_tests.jl
+++ test/regression_tests.jl
@@ -47,13 +47,14 @@
 
     index_age = 60
 
-    # Test 1: Standard case
+    # Test 1: Standard case, check regression and criteria_selection at the same time
     reg = regression(:h, :age, data_age) |> criteria_selection
 
     expected_site = [
-      20.5, 20.3, 21.5, 20.4, 20.4, 22.2, 20.3, 21.0, 19.9, 20.0,
-      21.4, 19.9, 21.2, 20.1, 20.0, 20.0, 20.5, 20.8, 19.8, 21.6,
-      19.5, 19.7, 20.3, 19.3, 20.5, 19.5, 20.3, 21.3, 20.2, 21.0]
+      20.5, 20.4, 21.5, 20.7, 20.3, 21.1, 20.4, 21.0, 20.0, 19.6,
+      20.8, 20.1, 21.2, 20.3, 19.6, 20.4, 20.5, 20.8, 19.7, 22.8,
+      20.2, 20.0, 20.3, 19.1, 20.6, 20.2, 20.4, 21.3, 20.4, 21.5
+    ]
 
     @test site_classification(reg, data_age, index_age) ≈ expected_site atol = 1e-6
     @test site_classification(reg, index_age) ≈ expected_site atol = 1e-6
@@ -69,7 +70,7 @@
 
     expected_height = [
       13.6, 17.8, 21.5, 21.5, 21.8, 14.3, 17.8, 21.0, 21.0, 21.4,
-      14.0, 17.5, 21.2, 21.2, 21.4, 13.4, 18.0, 20.8, 20.9, 23.2,
+      13.9, 17.5, 21.2, 21.2, 21.4, 13.5, 18.0, 20.8, 20.8, 23.2,
       13.2, 17.4, 20.3, 20.3, 22.0, 13.2, 17.8, 21.3, 21.3, 22.5
     ]
 
